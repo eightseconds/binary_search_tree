c1dc38fe4f5783fb70daa6e86483cf46d19a02c4
eightseconds <paulseokcho@gmail.com>
2019-10-09 19:14:34 -0700

    Implement constructor and functions

---
+++ binarysearchtree.js
@@ -0,0 +1,47 @@
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(value) {
+        if (value < this.value) {
+            if (this.left === null) {
+                this.left = new BST(value)
+            } else {
+                this.left.insert(value)
+            }
+        } else {
+            if (this.right === null) {
+                this.right = new BST(value)
+            } else {
+                this.right.insert(value)
+            }
+        }
+        return this;
+    }
+
+    contains(value) {
+        if (value < this.value) {
+            if (this.left === null) {
+                return false
+            } else {
+                return this.left.contains(value)
+            }
+        } else if (value > this.value) {
+            if (this.right === null) {
+                return false
+            } else {
+                return this.right.contains(value)
+            }
+        } else {
+            return true;
+        }
+    }
+
+    remove(value) {
+
+        return this;
+    }
+}
